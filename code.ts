9838bb00d8233ce4d754de6c6d218062627342ae
Oscar Otero <oom@oscarotero.com>
2021-06-24 20:20:07 +0200

    advances

--- code.ts
+++ code.ts
@@ -1,39 +1,111 @@
-figma.showUI(__html__);
+// const textSelection = figma.currentPage.selection.filter((i) => i.type === "TEXT");
+let nodes = figma.root.findAll((node) => node.type === "TEXT") as TextNode[];
+let nodeInfo: Info[];
 
-figma.ui.onmessage = async msg => {
+interface Info {
+  name: string;
+  oldCopy: string;
+  copy: string;
+  vars: { [index: string]: string };
+  node: TextNode;
+}
+
+figma.showUI(__html__, {
+  width: 500,
+  height: 500,
+});
+
+figma.ui.onmessage = async (msg) => {
   switch (msg.type) {
-    case 'update':
-      await updateCopies(msg);
+    case "update":
+      await searchCopies(msg);
+      break;
+    case "info":
+      await updateInfo(msg);
+      break;
+    case "cancel":
+      figma.closePlugin();
       break;
   }
-
-  figma.closePlugin();
 };
 
-async function updateCopies(msg) {
+async function searchCopies(msg) {
   const { copies } = msg;
-  const textNodes = figma.root.findAll((node) => node.type === "TEXT");
+  nodes = nodes.filter((node) => (node.name in copies));
+  nodeInfo = nodes.map((node: TextNode) => getInfo(node, copies[node.name]));
 
-  for (const node of textNodes) {
-    if (node.type !== "TEXT") {
-      continue;
-    }
+  const requiredInfo = nodeInfo.filter((info) => Object.keys(info.vars));
 
-    const { name } = node;
+  if (requiredInfo.length) {
+    figma.showUI(__html__);
 
-    if (!(name in copies)) {
-      continue;
-    }
-
-    const ranges = getRanges(node, copies[name])
-    ranges.forEach(async ({start, end, fontName, newCopy}) => {
-      await figma.loadFontAsync(fontName as FontName);
-
-      node.insertCharacters(start, newCopy, "AFTER");
-      node.deleteCharacters(start + newCopy.length, end + newCopy.length);
-      console.log({start, end, newCopy});
-    })
+    figma.ui.postMessage({
+      type: "info",
+      nodes: requiredInfo,
+    });
+  } else {
+    await updateCopies();
+    figma.closePlugin();
   }
+}
+
+async function updateCopies() {
+  for (const { node, copy, vars } of nodeInfo) {
+    try {
+      const newCopy = copy.replaceAll(
+        /\{\{\s*([^\}]+)\s*\}\}/g,
+        (match, name) => vars[name] || match,
+      );
+      const ranges = getRanges(node, newCopy);
+
+      for (const { start, end, fontName, newCopy } of ranges) {
+        await figma.loadFontAsync(fontName as FontName);
+
+        node.insertCharacters(start, newCopy, "AFTER");
+        node.deleteCharacters(start + newCopy.length, end + newCopy.length);
+      }
+    } catch (error) {
+      figma.ui.postMessage({
+        type: "error",
+        message: error.message,
+        node: node.name,
+        copy: copy,
+      });
+    }
+  }
+}
+
+async function updateInfo(msg) {
+  for (const [id, value] of msg.entries) {
+    const [nodeId, name] = id.split("-");
+    const node = nodeInfo.find((info) => info.node.id === nodeId);
+
+    if (node) {
+      node.node.setPluginData(name, value);
+      node.vars[name] = value;
+    }
+  }
+
+  await updateCopies();
+  figma.closePlugin();
+}
+
+function getInfo(node: TextNode, copy: string) {
+  const info = {
+    name: node.name,
+    oldCopy: node.characters,
+    copy,
+    vars: {},
+    node,
+  };
+
+  const matches = copy.matchAll(/\{\{\s*([^\}]+)\s*\}\}/g);
+
+  for (const [_, name] of matches) {
+    info.vars[name] = node.getPluginData(name).trim();
+  }
+
+  return info;
 }
 
 function getRanges(node, copy) {
@@ -71,7 +143,7 @@ function getNodeRanges(node) {
     const [fontName, id] = getChartStyle(node, start);
     if (id !== latest) {
       latest = id;
-      ranges.push({start, fontName});
+      ranges.push({ start, fontName });
     }
     ++start;
   }
@@ -86,10 +158,12 @@ function getChartStyle(node, index) {
   const textDecoration = node.getRangeTextDecoration(index, index + 1);
   const fills = node.getRangeFills(index, index + 1);
 
-  return [fontName, JSON.stringify([fontSize, fontName, textCase, textDecoration, fills])];
+  return [
+    fontName,
+    JSON.stringify([fontSize, fontName, textCase, textDecoration, fills]),
+  ];
 }
 
 function getCopyRanges(html) {
-  const ranges = html.split(/<[^>]+>/).filter((value) => value !== "");
-  return ranges;
+  return html.split(/<[^>]+>/).filter((value) => value !== "");
 }
